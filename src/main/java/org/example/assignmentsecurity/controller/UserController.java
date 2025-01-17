7d3fe29e1e01eb9c9c866d217782a866d4784d70
dbrjsdn2051 <dbrjdn2051@naver.com>
2025-01-17 18:16:54 +0900

    :sparkles: [#15] FEAT(UserController) : 회원 정보 조회 Controller 구현

--- src/main/java/org/example/assignmentsecurity/controller/UserController.java
+++ src/main/java/org/example/assignmentsecurity/controller/UserController.java
@@ -2,11 +2,15 @@ package org.example.assignmentsecurity.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.assignmentsecurity.common.format.ApiResult;
+import org.example.assignmentsecurity.config.AuthUser;
 import org.example.assignmentsecurity.controller.dto.rep.UserCreateRepDto;
 import org.example.assignmentsecurity.controller.dto.resp.UserCreateRespDto;
+import org.example.assignmentsecurity.controller.dto.resp.UserInfoRespDto;
 import org.example.assignmentsecurity.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +24,13 @@ public class UserController {
     public ResponseEntity<ApiResult<UserCreateRespDto>> signin(@RequestBody UserCreateRepDto dto) {
         UserCreateRespDto respDto = userService.register(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResult.success(respDto));
+    }
+
+    @GetMapping("/api/users")
+    public ResponseEntity<ApiResult<UserInfoRespDto>> userInfo(
+            @AuthenticationPrincipal AuthUser authUser
+    ) {
+        UserInfoRespDto respDto = userService.findUser(authUser.getNickname());
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.success(respDto));
     }
 }
