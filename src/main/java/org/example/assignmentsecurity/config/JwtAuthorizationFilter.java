5c2274bcdc8b87e25a626406f1a619ddf13ead53
dbrjsdn2051 <dbrjdn2051@naver.com>
2025-01-17 16:46:42 +0900

    :sparkles: [#5] FEAT(JwtAuthorizationFilter) : 인가 필터 구현

---
+++ src/main/java/org/example/assignmentsecurity/config/JwtAuthorizationFilter.java
@@ -0,0 +1,43 @@
+package org.example.assignmentsecurity.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.assignmentsecurity.common.error.ErrorCode;
+import org.example.assignmentsecurity.common.error.SecurityFilterChainException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getRequestURI().contains("/auth")) {
+            filterChain.doFilter(request, response);
+        }
+
+        String tokenValue = request.getHeader(JwtProvider.AUTHENTICATION_HEADER_PREFIX);
+
+        if (tokenValue == null) {
+            throw new SecurityFilterChainException(ErrorCode.TOKEN_NOT_FOUND);
+        }
+
+        String token = tokenValue.replace(JwtProvider.TOKEN_PREFIX, "");
+        jwtProvider.validateToken(token);
+
+        AuthUser authUser = jwtProvider.getAuthUserForToken(token);
+
+        LoginAuthentication loginAuthentication = new LoginAuthentication(authUser);
+        SecurityContextHolder.getContext().setAuthentication(loginAuthentication);
+        filterChain.doFilter(request, response);
+    }
+
+
+}
